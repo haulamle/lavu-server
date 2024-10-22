dd814455e37b2b9f0f001a1a5b46010aa0b11d40
haulamle <haulamle@gmail.com>
2024-10-22 16:01:14 +0700

    add pagination suplier

--- src/controllers/supplier.ts
+++ src/controllers/supplier.ts
@@ -1,17 +1,26 @@
 import SupplierModel from "../models/SupplierModel";
 
-// const getProducts = async (req: any, res: any) => {
-//   try {
-//     res.status(200).json({
-//       data: [],
-//       message: "Get products successfully!!!",
-//     });
-//   } catch (error: any) {
-//     res.status(404).json({
-//       message: error.message,
-//     });
-//   }
-// };
+const getSuppliers = async (req: any, res: any) => {
+  const { page, pageSize } = req.query;
+  try {
+    const skip = (Number(page) - 1) * Number(pageSize);
+    const items = await SupplierModel.find({ isDeleted: false })
+      .skip(skip)
+      .limit(Number(pageSize));
+    const total = await SupplierModel.countDocuments({ isDeleted: false });
+    res.status(200).json({
+      data: {
+        items,
+        total,
+      },
+      message: "Get suppliers successfully!!!",
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
 
 const addNew = async (req: any, res: any) => {
   const body = req.body;
@@ -29,4 +38,35 @@ const addNew = async (req: any, res: any) => {
     });
   }
 };
-export { addNew };
+
+const update = async (req: any, res: any) => {
+  const body = req.body;
+  const { id } = req.query;
+  try {
+    await SupplierModel.findByIdAndUpdate(id, body);
+
+    res.status(200).json({
+      data: [],
+      message: "update supplier successfully!!!",
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+const removeSupplier = async (req: any, res: any) => {
+  const { id } = req.query;
+  try {
+    await SupplierModel.findByIdAndUpdate(id, { isDeleted: true });
+    res.status(200).json({
+      data: [],
+      message: "delete supplier successfully!!!",
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+export { addNew, getSuppliers, update, removeSupplier };
--- src/routers/supplier.ts
+++ src/routers/supplier.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { addNew } from "../controllers/supplier";
+import {
+  addNew,
+  getSuppliers,
+  removeSupplier,
+  update,
+} from "../controllers/supplier";
 
 const router = Router();
 
-router.get("/");
+router.get("/", getSuppliers);
 router.post("/add-new", addNew);
+router.put("/update", update);
+router.put("/remove", removeSupplier);
 
 export default router;
