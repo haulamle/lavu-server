f878633f9ff528a34afc5746ad05204095e0f44d
haulamle <haulamle@gmail.com>
2024-10-26 14:01:22 +0700

    get api excel data

--- src/controllers/supplier.ts
+++ src/controllers/supplier.ts
@@ -1,4 +1,5 @@
 import SupplierModel from "../models/SupplierModel";
+import { supplierForm } from "../forms/supplier";
 
 const getSuppliers = async (req: any, res: any) => {
   const { page, pageSize } = req.query;
@@ -69,4 +70,59 @@ const removeSupplier = async (req: any, res: any) => {
     });
   }
 };
-export { addNew, getSuppliers, update, removeSupplier };
+
+const getForm = async (req: any, res: any) => {
+  try {
+    const form = {
+      title: "Supplier",
+      layout: "horizontal",
+      labelCol: 6,
+      wrapperCol: 18,
+      formItems: supplierForm,
+    };
+
+    res.status(200).json({
+      message: "",
+      data: form,
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+
+const getExportData = async (req: any, res: any) => {
+  const body = req.body;
+  const { start, end } = req.query;
+  const filter: any = {};
+  if (start && end) {
+    filter.createdAt = {
+      $gte: new Date(start),
+      $lte: new Date(end),
+    };
+  }
+  try {
+    const items = await SupplierModel.find(filter);
+    const data: any = [];
+    if (items.length > 0) {
+      items.forEach((item: any) => {
+        const value: any = {};
+        body.forEach((key: any) => {
+          value[key] = item[key] ? item[key] : "";
+        });
+        data.push(value);
+      });
+    }
+    res.status(200).json({
+      data,
+      message: "export suppliers successfully!!!",
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+
+export { addNew, getSuppliers, update, removeSupplier, getForm, getExportData };
